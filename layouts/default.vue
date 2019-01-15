ca4da4cb305e44c2d0142599fa78b01bf540c329
Satoshi Innami <innamisatoshi@gmail.com>
2019-01-15 22:10:07 +0900

    initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,65 @@
+<template>
+  <v-app>
+    <v-toolbar
+      :clipped-left="clipped"
+      color="white"
+      fixed
+      app
+    >
+      <v-toolbar-title v-text="title"/>
+      <v-spacer />
+      <v-btn
+        icon
+        @click.stop="rightDrawer = !rightDrawer"
+      >
+        <v-icon>settings</v-icon>
+      </v-btn>
+    </v-toolbar>
+    <v-content>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-content>
+    <v-navigation-drawer
+      :right="right"
+      v-model="rightDrawer"
+      temporary
+      fixed
+    >
+      <v-list>
+        <v-list-tile @click.native="right = !right">
+          <v-list-tile-action>
+            <v-icon light>compare_arrows</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-title>Switch drawer (click me)</v-list-tile-title>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-footer
+      :fixed="fixed"
+      app
+    >
+      <span>&copy; SHARE PAINT 2019</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        clipped: false,
+        drawer: true,
+        fixed: false,
+        items: [
+          { icon: 'apps', title: 'Welcome', to: '/' },
+          { icon: 'bubble_chart', title: 'Inspire', to: '/inspire' }
+        ],
+        miniVariant: false,
+        right: true,
+        rightDrawer: false,
+        title: 'SHARE PAINT'
+      }
+    }
+  }
+</script>
